854268e76306a0c4f027ad5f235506a7f4eb1e1c
19509 <19509@my4county.net>
2022-01-11 14:53:33 -0500

    just some changes

--- Assets/Scripts/UI/SensitivityValue.cs
+++ Assets/Scripts/UI/SensitivityValue.cs
@@ -6,20 +6,17 @@ using UnityEngine.UI;
 
 public class SensitivityValue : MonoBehaviour
 {
-    Text newText;
+    public Text newText;
     public Slider slider;
-    public Toggle toggleRun;
-    public Toggle toggleCrouch;
-    public Toggle toggleBob;
+
+
+    
 
     private void Start()
     {
-        newText = GetComponent<Text>();
-        newText.text = (10f + "");
-        slider.value = 10f;
-        toggleRun.isOn = SharedValues.sharedTogRun;
-        toggleCrouch.isOn = SharedValues.sharedTogCrouch;
-        toggleBob.isOn = SharedValues.headBob;
+        
+        newText.text = ((PlayerPrefs.GetFloat("sharedSens")*1000) + "");
+        slider.value = (PlayerPrefs.GetFloat("sharedSens")*1000);
     }
 
     public void textUpdate(float value)
@@ -27,5 +24,6 @@ public class SensitivityValue : MonoBehaviour
         FindObjectOfType<AudioManager>().Play("Click");
         newText.text = (Math.Round(value, 2) + "");
     }
+
 
 }
